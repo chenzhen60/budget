84e6c2cfa47c8ea498a8792227f7c7c4a0afe1fa
chenzhen <1530839114@qq.com>
2019-06-26 14:55:48 +0800

    p

---
+++ front_end/budget/budget/view/MoneyBudgetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoneyBudgetTableViewCell.swift
+//  budget
+//
+//  Created by chenzhen on 2019/6/26.
+//  Copyright © 2019 chenzhen. All rights reserved.
+//
+
+import UIKit
+
+class MoneyBudgetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
